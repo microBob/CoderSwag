478a3061f58234f41955b90ebf5e85bc687308d6
Microbob µ <microbob@is88.com>
2017-10-08 12:01:22 -0400

    vid 3

---
+++ CoderSwag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Kenneth on 10/8/17.
+//  Copyright © 2017 Microbob. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+	@IBOutlet weak var productImage: UIImageView!
+	@IBOutlet weak var productTitle: UILabel!
+	@IBOutlet weak var productPrice: UILabel!
+}
